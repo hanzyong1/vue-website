2990f3fbe2581c45e8c4721571990ebc04b0056f
Han <hanzyong1@gmail.com>
2023-07-07 17:12:46 +0800

    add search function in show blogs

--- src/views/BlogsView.vue
+++ src/views/BlogsView.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="show-blogs">
     <h1>Blog Articles</h1>
-    <div v-for="blog in blogs" :key="blog.id" class="single-blog">
+    <input type="text" v-model="search" placeholder="Search blogs" />
+    <div v-for="blog in filteredBlogs" :key="blog.id" class="single-blog">
       <h2>{{ blog.title | capitalize }}</h2>
       <article>{{ blog.body | truncate }}</article>
     </div>
@@ -15,6 +16,7 @@ export default {
   data() {
     return {
       blogs: [],
+      search: "",
     };
   },
   async created() {
@@ -27,6 +29,13 @@ export default {
       console.log(error);
     }
   },
+  computed: {
+    filteredBlogs: function () {
+      return this.blogs.filter((blog) => {
+        return blog.title.match(this.search);
+      });
+    },
+  },
 };
 </script>
 
@@ -34,11 +43,16 @@ export default {
 #show-blogs {
   max-width: 800px;
   margin: 0px auto;
+  text-align: left;
 }
 .single-blog {
   padding: 20px;
   margin: 20px 0;
-  box-sizing: border-box;
   background: #eee;
+}
+
+input {
+  width: 100%;
+  padding: 10px;
 }
 </style>
